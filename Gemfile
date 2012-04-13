367907998d3a989bd35fd0771f1199147d328c14
Igor Afonov <afonov@gmail.com>
2012-04-13 17:22:43 +0300

    Remove Everything.

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source :rubygems
-
-gem "cucumber", "~> 1.1.9"
-gem "chef"
-gem "ruby-debug"
--- features/step_definitions/steps.rb
+++
@@ -1,5 +0,0 @@
-When /^I install "([^"]*)" package$/ do |package_name|
-  execute "test command" do
-    command "echo 0"
-  end
-end
